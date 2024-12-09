d46084904831380fde463eed3e2f14cfe16c78fc
Forestest <hosting0web258@gmail.com>
2024-12-09 20:52:53 +0500

    Fix projects bug

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,11 +3,6 @@ import { motion } from 'framer-motion';
 import ProjectCard from '../components/ProjectCard';
 import { sampleProjects } from '../data/projects';
 import { AnimatedModalDemo } from './Test';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect } from 'react';
-
-gsap.registerPlugin(ScrollTrigger) 
 
 
 
@@ -16,49 +11,6 @@ const Projects = () => {
 
 
 
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.1,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { y: 20, opacity: 0 },
-    visible: { y: 0, opacity: 1 },
-  };
-
-
- useEffect(()=>{
-  let tl = gsap.timeline({
-    // yes, we can add it to an entire timeline!
-    scrollTrigger: {
-        trigger: '#contein',
-        pin: true, // pin the trigger element while active
-        start: 'top top', // when the top of the trigger hits the top of the viewport
-        end: '+=500', // end after scrolling 500px beyond the start
-        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-        snap: {
-            snapTo: 'labels', // snap to the closest label in the timeline
-            duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-            ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
-        }
-    }
-});
-
-// add animations and labels to the timeline
-tl.addLabel('start')
-    .from('#hero_section', { scale: 0.3, rotation: 45, autoAlpha: 0 })
-    .addLabel('color')
-    .from('#projects_section', { backgroundColor: '#28a92b' })
-    .addLabel('spin')
-    .to('#cta_section', { rotation: 360 })
-    .addLabel('end');
- })
 
   return (
     <div id='contein' className="min-h-screen pt-16 bg-black text-gray-200">
